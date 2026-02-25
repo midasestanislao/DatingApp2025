db88a29a98918756ca4e5eb22093eb897e61b360
Gustavo Richarte <midasestanislao@users.noreply.github.com>
2026-02-25 07:18:20 -0600

    Parcial04 (#9)
    * Parcail04 - Upgrade to .NET 10
    
    * Parcial04 - Add edit profile button
    
    * Parcial04 - Update ssl
    
    * Parcial04 - Add member edition
    
    * Parcial04 - Add prevent unsave changes guard
    
    * Parcial04 - Add member update pesistance
    
    * Parcial04 - Add ClaimsPrincipal extension method
    
    * Parcial04 - Add member update in client and small fix
    
    * Parcial04 - Enhance member edit form
    
    * Parcial04 - Add loading interceptor
    
    * Parcial04 - Add memory cache
    
    * Parcial04 - Add Cloudinary config and Photo service
    
    * Parcial04 - Add photo upload
    
    * Parcial04 - Add photo upload feature
    
    * Parcial04 - Add set main photo in API
    
    * Parcial04 - Add main photo selection
    
    * Parcial04 - Add reusable icon button
    
    * Parcial04 - Small cosmetic fixes
    
    * Parcial04 - Add delete photo from API
    
    * Parcial04 - Add delete photo from client

--- client/src/features/member-profile/member-profile.ts
+++ client/src/features/member-profile/member-profile.ts
@@ -1,21 +1,64 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../types/member';
+import { Component, HostListener, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { EditableMember, Member } from '../../types/member';
 import { DatePipe } from '@angular/common';
+import { MembersService } from '../../core/services/members-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../core/services/toast-service';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
-export class MemberProfile implements OnInit {
-  private route = inject(ActivatedRoute);
-  protected member = signal<Member | undefined>(undefined);
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('memberProfileEditForm') memberProfileEditForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify ($event:BeforeUnloadEvent) {
+    if (this.memberProfileEditForm?.dirty) {
+      $event.preventDefault();
+    }
+  };
+  private accountService = inject(AccountService);
+  private toast = inject(ToastService);
+  protected membersService = inject(MembersService);
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: ''
+  };
 
   ngOnInit(): void {
-    this.route.parent?.data.subscribe(data => {
-      this.member.set(data["member"]);
-    })
+    this.editableMember = {
+      displayName: this.membersService.member()?.displayName || '',
+      description: this.membersService.member()?.description || '',
+      city: this.membersService.member()?.city || '',
+      country: this.membersService.member()?.country || ''
+    };
+  }
+
+  ngOnDestroy(): void {
+    if (this.membersService.editMode()) {
+      this.membersService.editMode.set(false);
+    }
+  }
+
+  updateProfile() {
+    if (!this.membersService.member()) return;
+    const updatedMember = {...this.membersService.member(), ...this.editableMember};
+    this.membersService.updateMember(this.editableMember).subscribe({
+      next: () => {
+        const currentUser = this.accountService.currentUser();
+        if (currentUser && updatedMember.displayName !== currentUser?.displayName) {
+          currentUser.displayName = updatedMember.displayName;
+          this.accountService.setCurrentUser(currentUser);
+        }
+        this.membersService.editMode.set(false);
+        this.membersService.member.set(updatedMember as Member);
+        this.memberProfileEditForm?.reset(updatedMember);
+        this.toast.success('Profile updated successfully');
+      }
+    });
   }
 }
